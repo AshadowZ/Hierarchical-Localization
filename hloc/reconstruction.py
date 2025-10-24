cef330b2958056340f130f940ff02be986d5edee
x299 <1678807086@qq.com>
2025-10-24 13:21:07 +0800

    [Feat] Add XFeat + LightGlue and sequential matching, referencing https://github.com/cvg/Hierarchical-Localization/pull/441

--- hloc/reconstruction.py
+++ hloc/reconstruction.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import argparse
 import multiprocessing
 import shutil
@@ -15,7 +16,7 @@ from .triangulation import (
     import_matches,
     parse_option_args,
 )
-from .utils.io import open_colmap_database
+from .utils.database import COLMAPDatabase
 
 
 def create_empty_db(database_path: Path):
@@ -23,8 +24,10 @@ def create_empty_db(database_path: Path):
         logger.warning("The database already exists, deleting it.")
         database_path.unlink()
     logger.info("Creating an empty database...")
-    with open_colmap_database(database_path) as _:
-        pass
+    db = COLMAPDatabase.connect(database_path)
+    db.create_tables()
+    db.commit()
+    db.close()
 
 
 def import_images(
@@ -51,9 +54,11 @@ def import_images(
 
 
 def get_image_ids(database_path: Path) -> Dict[str, int]:
+    db = COLMAPDatabase.connect(database_path)
     images = {}
-    with open_colmap_database(database_path) as db:
-        images = {image.name: image.image_id for image in db.read_all_images()}
+    for name, image_id in db.execute("SELECT name, image_id FROM images;"):
+        images[name] = image_id
+    db.close()
     return images
 
 
@@ -167,16 +172,15 @@ def main(
     create_empty_db(database)
     import_images(image_dir, database, camera_mode, image_list, image_options)
     image_ids = get_image_ids(database)
-    with open_colmap_database(database) as db:
-        import_features(image_ids, db, features)
-        import_matches(
-            image_ids,
-            db,
-            pairs,
-            matches,
-            min_match_score,
-            skip_geometric_verification,
-        )
+    import_features(image_ids, database, features)
+    import_matches(
+        image_ids,
+        database,
+        pairs,
+        matches,
+        min_match_score,
+        skip_geometric_verification,
+    )
     if not skip_geometric_verification:
         estimation_and_geometric_verification(database, pairs, verbose)
     reconstruction = run_reconstruction(
